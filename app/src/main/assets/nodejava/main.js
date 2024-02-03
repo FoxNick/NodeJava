22294a60c71c949d02eb8558093b3b1fd0458823
SuMuCheng <3578557729@qq.com>
2024-02-03 13:06:34 +0800

    Remove gyp files

--- app/src/main/assets/nodejava/main.js
+++ app/src/main/assets/nodejava/main.js
@@ -1,3 +1,18 @@
 require("./rhino").install();
 
 $java.setUnsafeReflectionEnabled(true);
+
+const ctx = com.mucheng.nodejava.MainActivity.currentMainActivity.get();
+
+class MyButton extends android.widget.Button {
+    constructor(context) {
+        super(context);
+    }
+
+    onDraw(canvas) {
+        console.log(canvas);
+    }
+}
+
+const btn = new MyButton(ctx);
+ctx.setContentView(btn);
